444a75be7b3cfe7f8c27c37eba03b979a2d8d509
jgcastrejona <jgcastrejona@gmail.com>
2015-11-02 16:13:42 -0600

    Primer Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# controlTienda
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/ProductosController.js
@@ -0,0 +1,11 @@
+/**
+ * ProductosController
+ *
+ * @description :: Server-side logic for managing productos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
